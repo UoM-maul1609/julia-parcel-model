2e9e91d6de75471784f8c7963c87ac84e7dfcfef
maul1609 <p.connolly@manchester.ac.uk>
2023-07-06 12:06:06 +0100

    testing different ODE solvers

--- example_ode.jl
+++ example_ode.jl
@@ -17,10 +17,33 @@ tspan = (0.0,100.0)
 prob = ODEProblem(lorenz!,u0,tspan)
 
 # Test that it worked
+# https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/#Translations-from-MATLAB/Python/R
+
 using OrdinaryDiffEq
 
-sol = solve(prob,Tsit5())
+
+sol = solve(prob,Tsit5(),abstol=1.e-10, reltol=1.e-15)
 
 # import Pkg; Pkg.add("Plots")
-using Plots; plot(sol,idxs=(1,2,3))
+using Plots; 
+#plot(sol,idxs=(1,2,3), c=:red)
 
+# stiff method, like VODE
+prob = ODEProblem(lorenz!,u0,tspan)
+
+sol = solve(prob,QNDF(),abstol=1.e-10, reltol=1.e-15)
+plot(sol,idxs=(1,2,3), c=:green)
+
+# stiff method, like VODE
+prob = ODEProblem(lorenz!,u0,tspan)
+
+sol = solve(prob,FBDF(),abstol=1.e-10, reltol=1.e-15)
+plot!(sol,idxs=(1,2,3), c=:blue)
+
+# Pkg.add("LSODA")
+using LSODA
+# stiff method, like lsoda
+prob = ODEProblem(lorenz!,u0,tspan)
+
+sol = solve(prob,lsoda(),abstol=1.e-10, reltol=1.e-15)
+plot!(sol,idxs=(1,2,3), c=:black)
